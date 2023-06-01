2ead69f7ab405623c52a1de41dd487d5a2e428c1
AsafArevir <100048373+AsafArevir@users.noreply.github.com>
2023-05-31 20:56:36 -0600

    Add files via upload
    Unzip rar

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 30,
+        host: 'localhost',
+        user: 'root',
+        password: 'Nolose123$',
+        database: 'mexicompras'
+    }
+
+};
